584f879f759becd84249e42fc2123c5d8d625fdd
nurtileksergey <sergeinurtilek@gmail.com>
2025-10-26 17:27:23 +0500

    Merge remote-tracking branch 'origin/feature/prim' into feature/prim

---
+++ src/main/java/metrics/PerformanceTracker.java
@@ -0,0 +1,43 @@
+package metrics;
+
+public class PerformanceTracker {
+    private long comparisons = 0;
+    private long swaps = 0;
+    private long arrayAccesses = 0;
+
+    public void incrementComparisons() {
+        this.comparisons++;
+    }
+
+    public void incrementSwaps() {
+        this.swaps++;
+    }
+
+    public void incrementArrayAccesses(int count) {
+        this.arrayAccesses += count;
+    }
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public long getSwaps() {
+        return swaps;
+    }
+
+    public void reset() {
+        this.comparisons = 0;
+        this.swaps = 0;
+        this.arrayAccesses = 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Comparisons: %d, Swaps: %d, Array Accesses: %d",
+                comparisons, swaps, arrayAccesses);
+    }
+
+    public String toCsv() {
+        return String.format("%d,%d,%d", comparisons, swaps, arrayAccesses);
+    }
+}
